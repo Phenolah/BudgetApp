53d1b76407f01b1d46d68adbda3c85efb94543fa
Phenolah <phenolaha@gmail.com>
2023-10-11 02:19:13 -0700

    Registration Views

--- BudgetProject/BudgetProject/authentication/views.py
+++ BudgetProject/BudgetProject/authentication/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from .serializers import UserSerializer
+from rest_framework.generics import GenericAPIView
+from rest_framework import status
+from rest_framework.response import Response
+
 
 # Create your views here.
+
+class RegisterViews(GenericAPIView):
+    serializer_class = UserSerializer
+    
+    def post(self, request):
+        serializer = UserSerializer(data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data,status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    
+    
+    
