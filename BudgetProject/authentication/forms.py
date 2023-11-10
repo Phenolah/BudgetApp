2ca7edc2dfbbe247f014158dd441c43cc2bc21f2
Phenolah <phenolaha@gmail.com>
2023-11-09 23:34:05 -0800

    Setting up Registration

--- BudgetProject/authentication/forms.py
+++ BudgetProject/authentication/forms.py
@@ -1,1 +1,16 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+class UserRegistrationForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']
+
+class UserLoginForm(forms.Form):
+    username = forms.CharField(max_length=100)
+    password = forms.CharField(max_length=100,
+                               label='password',
+                               widget=forms.PasswordInput(attrs={'placeholder': 'Enter your password:'}))
+
 
