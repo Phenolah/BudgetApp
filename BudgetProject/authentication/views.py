74db99a7fda5ef577bad0f5381a94dc3391295b5
Phenolah <phenolaha@gmail.com>
2023-11-10 00:26:01 -0800

    Add registration with GET and POST handling

--- BudgetProject/authentication/views.py
+++ BudgetProject/authentication/views.py
@@ -1,23 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .serializers import UserSerializer
 from rest_framework.generics import GenericAPIView
 from rest_framework import status
 from rest_framework.response import Response
+from .forms import *
+from django.views.decorators.http import require_http_methods
+from django.contrib.auth import login
+from django.contrib import messages
 
 
 # Create your views here.
+@require_http_methods(["GET", "POST"])
+def RegistrationView(request):
+    template_name = 'authentication/registration.html'
 
-class RegisterView(GenericAPIView):
-    serializer_class = UserSerializer
-
-    def post(self, request):
-        serializer = UserSerializer(data=request.data)
-
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
+    if request.method == 'POST':
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            user = form.save()
+            login(request,user)
+            messages.sucess(request, f"Hey {username}, your account has been created")
+            return redirect('home')
         else:
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    def get(self, request):
-        serializer = UserSerializer()
-        return render(request, 'authentication/registration.html', {serializer:'serializer'})
+            form = UserRegistrationForm()
+            context = {
+                'form': form
+            }
+            return render(request, template_name , context)
